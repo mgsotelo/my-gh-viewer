ab27a27135c4058358c7061916bc3032336c0117
Mario Sotelo <mgsotelo@pucp.pe>
2021-01-07 11:34:00 -0500

    doing some views
    not too much yet

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,29 @@
 <template>
-  <div>
+  <b-container>
+    <b-row class="myheader">
+      <h1><a :href="'https://github.com/'+theRepo()"> {{theRepo()}} </a></h1>
+    </b-row>
 
-  </div>
+    <b-row>
+      <b-col cols="3">asdasd</b-col>
+      <b-col cols="9"> xdxdd </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  methods: {
+    theRepo() {
+      return 'mgsotelo/my-gh-viewer';
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+.myheader {
+  background-color: #d3d3f2;
+  margin-bottom: 1%;
+}
 </style>
