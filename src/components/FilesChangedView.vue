698f149e82874c81e6dbd57c619c7983bd3568fd
Mario Sotelo <mgsotelo@pucp.pe>
2021-01-07 17:00:44 -0500

    FileChangedView v0.0.1

--- src/components/FilesChangedView.vue
+++ src/components/FilesChangedView.vue
@@ -1,15 +1,39 @@
 <template>
   <div>
-
+    <h2>Files changed</h2>
+    <span>{{ sha }}</span>
+    <div class="scrollme">
+      <div v-for="file in files" :key="file.id">
+        {{ file }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {
+  props: {
+    files: {
+      type: Array,
+      default: () => [],
+    },
+
+    sha: {
+      type: String,
+      default: '',
+    },
+  },
+};
 </script>
 
 <style scoped>
+span {
+  font-size: 12px;
+}
+
+.scrollme {
+  max-height: 584px;
+  overflow: scroll;
+}
 
 </style>
